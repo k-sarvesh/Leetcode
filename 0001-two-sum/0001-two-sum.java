91cfb9129e20b62e432f96dc69e41df1c078ac74
Sarvesh <sarveshkhawale17@gmail.com>
2026-08-14 23:48:42 +0530

    Time: 45 ms (26.97%), Space: 46.8 MB (85.37%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int[] twoSum(int[] arr, int target) {
+            for(int i = 0 ; i < arr.length -1 ; i++){
+            for(int j = i+1 ; j < arr.length;j++){
+                int sum = arr[i]+arr[j];
+                if(sum==target){
+                    return new int[]{i,j};
+                }
+            }
+        }
+        return new int[]{-1,-1};
+    }
+    }
+    
