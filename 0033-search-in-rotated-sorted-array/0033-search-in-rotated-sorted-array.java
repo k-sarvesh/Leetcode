69349838e44b19cdc52964ad4575447e68b85091
Sarvesh <sarveshkhawale17@gmail.com>
2026-09-16 19:12:58 +0530

    Time: 0 ms (100%), Space: 43.8 MB (65.82%) - LeetHub

---
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -0,0 +1,62 @@
+class Solution {
+    public int search(int[] arr, int target) {
+        int start = 0;
+        int end = arr.length - 1;
+        int pivot = findPivot(arr);
+
+        if (pivot == -1) {
+            // Array is not rotated
+            return binarySearch(arr, target, start, end);
+        }
+
+        if (arr[pivot] == target) {
+            return pivot;
+        }
+
+        if (target >= arr[0]) {
+            return binarySearch(arr, target, 0, pivot - 1);
+        }
+
+        return binarySearch(arr, target, pivot + 1, end);
+    }
+
+    public int findPivot(int[] arr) {
+        int start = 0;
+        int end = arr.length - 1;
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+
+            if (mid < end && arr[mid] > arr[mid + 1]) {
+                return mid;
+            }
+            if (mid > start && arr[mid] < arr[mid - 1]) {
+                return mid - 1;
+            }
+
+            if (arr[mid] >= arr[start]) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+
+        return -1;
+    }
+
+    public int binarySearch(int[] arr, int target, int start, int end) {
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+
+            if (arr[mid] == target) {
+                return mid;
+            } else if (arr[mid] > target) {
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }
+
+        return -1;
+    }
+}
